1e6d1f7b1614c668207e5a101e54760445cfd173
Paul Cleary <paul.cleary@phe.gov.uk>
2019-11-17 11:43:55 +0000

    First working version

--- read_pdf.r
+++ read_pdf.r
@@ -1,23 +1,45 @@
 # read_pdf.r
 
-library(pdftools)
-library(stringr)
-
-# Function to create title from one file
-suggest_title <- function(filename, start_word=1, n_words=14) {
-  pdf_text <- paste(pdftools::pdf_text(filename), collapse='_')
-  pdf_text <- trimws(gsub('[[:punct:] ]+',' ', pdf_text))
-  pdf_text <- gsub('( [0-9] )', ' ', pdf_text)
-  pdf_text <- gsub('\\r|\\n', ' ', pdf_text)
-  pdf_text <- gsub('[[:space:] ]+',' ', pdf_text)
-  first_words <- stringr::word(pdf_text, start=start_word, end=n_words)
-  first_words
+sanitise_filename <- function(x) {
+  x <- gsub('[[:punct:] ]+', ' ', x)
+  x <- gsub('\\r|\\n', ' ', x)
+  x <- trimws(gsub('[[:space:] ]+', ' ', x))
+  x
 }
 
-suggest_title('test_files/18-0409.pdf')
-suggest_title('test_files/1-s2.0-S1473309916303863-main.pdf')
-suggest_title('test_files/10.1038@s41579-018-0115-z.pdf')
-suggest_title('test_files/769.full.pdf', 32, 50)
+n_words <- 20
+cat('\n')
+original_filenames <-
+  list.files('test_files/', pattern = '\\.pdf$|\\.PDF$', full.names = TRUE)
+for (o in original_filenames) {
+  o_dirname <- dirname(o)
+  o_basename <- basename(o)
+  cat('Original filename: ', o_basename, '\n')
+  o_title <- pdftools::pdf_info(o)$keys$Title
+  if (is.null(o_title)) {
+    raw_text <- paste0(pdftools::pdf_text(o), collapse = ' ')
+    raw_text_sane <- sanitise_filename(raw_text) 
+    o_title_sane <- stringr::word(raw_text_sane, 1, n_words)
+  } else  if (o_title %in% '') {
+    raw_text <- paste0(pdftools::pdf_text(o), collapse = ' ')
+    raw_text_sane <- sanitise_filename(raw_text) 
+    o_title_sane <- stringr::word(raw_text_sane, 1, 20)
+  } else { 
+    o_title_sane <- sanitise_filename(o_title)
+  }
+  cat('Suggested title: ', o_title_sane, '\n')
+  decision <- readline('Change to suggested title? (y/n): ')
+  if (decision %in% 'y') {
+    o_newname <-
+      file.path(o_dirname, paste0(o_title_sane, '.pdf', collapse = NULL))
+    file.rename(o, o_newname)
+    cat('File renamed to: ', o_newname, '\n')
+    cat('\n')
+  } else {
+    cat('File name not changed\n')
+    cat('\n')
+  }
+}
 
 # TODO
 # Shiny app
